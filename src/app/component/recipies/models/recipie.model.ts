8e8daeb4bf361dac22fcd9abf2f04b64c7118e0e
Ravi <ravzdhanju@gmail.com>
2022-03-05 21:16:58 -0500

    addded the models

--- src/app/component/recipies/models/recipie.model.ts
+++ src/app/component/recipies/models/recipie.model.ts
@@ -2,5 +2,11 @@ export class Recipie {
   public name: string;
   public description: string;
   public imagePath: string;
+  constructor(name: string, description: string, imagePath: string) {
+    this.name = name;
+    this.description = description;
+
+    this.imagePath = imagePath;
+  }
 }
 export default Recipie;
--- src/app/component/recipies/recipie-list/recipie-list.component.ts
+++ src/app/component/recipies/recipie-list/recipie-list.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+import Recipie from '../models/recipie.model';
 @Component({
   selector: 'app-recipie-list',
   templateUrl: './recipie-list.component.html',
-  styleUrls: ['./recipie-list.component.scss']
+  styleUrls: ['./recipie-list.component.scss'],
 })
 export class RecipieListComponent implements OnInit {
+  recipes: Recipie[] = [
+    new Recipie(
+      'Test Recipie',
+      'This is a test recipie',
+      'https://pinchofyum.com/wp-content/uploads/Homemade-Tomato-Soup-Square.png'
+    ),
+    new Recipie(
+      'Test Recipie',
+      'This is a test recipie',
+      'https://pinchofyum.com/wp-content/uploads/Homemade-Tomato-Soup-Square.png'
+    ),
+    new Recipie(
+      'Test Recipie',
+      'This is a test recipie',
+      'https://pinchofyum.com/wp-content/uploads/Homemade-Tomato-Soup-Square.png'
+    ),
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
