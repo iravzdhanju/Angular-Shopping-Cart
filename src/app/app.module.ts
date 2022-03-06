dac62238cc132f4a0f8cf491ea1dc4cb3b1c0e96
Ravi <ravzdhanju@gmail.com>
2022-03-05 20:25:49 -0500

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,19 @@ import { RecipiesComponent } from './component/recipies/recipies.component';
 import { RecipieListComponent } from './component/recipies/recipie-list/recipie-list.component';
 import { RecipieItemComponent } from './component/recipies/recipie-item/recipie-item.component';
 import { RecipieDetailComponent } from './component/recipies/recipie-item/recipie-detail/recipie-detail.component';
-
+import { ShoppingEditComponent } from './component/shopping-list/shopping-edit/shopping-edit.component';
+import { ShoppingListComponent } from './component/shopping-list/shopping-list.component';
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, RecipiesComponent, RecipieListComponent, RecipieItemComponent, RecipieDetailComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RecipiesComponent,
+    RecipieListComponent,
+    RecipieItemComponent,
+    RecipieDetailComponent,
+    ShoppingEditComponent,
+    ShoppingListComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/component/recipies/models/recipie.model.ts
@@ -0,0 +1,6 @@
+export class Recipie {
+  public name: string;
+  public description: string;
+  public imagePath: string;
+}
+export default Recipie;
