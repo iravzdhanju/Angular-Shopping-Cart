e8df062d3cf3097c8861efbf1b2f5d5fa14fb6b6
Ravi <ravzdhanju@gmail.com>
2022-03-05 20:01:00 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './component/header/header.component';
+import { RecipiesComponent } from './component/recipies/recipies.component';
+import { RecipieListComponent } from './component/recipies/recipie-list/recipie-list.component';
+import { RecipieItemComponent } from './component/recipies/recipie-item/recipie-item.component';
+import { RecipieDetailComponent } from './component/recipies/recipie-item/recipie-detail/recipie-detail.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, RecipiesComponent, RecipieListComponent, RecipieItemComponent, RecipieDetailComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
